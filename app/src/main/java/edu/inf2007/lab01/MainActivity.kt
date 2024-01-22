4cdf596089c5eb8d70dbe8c4a59860c133aa12bf
jolintanql <115628613+jolintanql@users.noreply.github.com>
2024-01-22 21:52:55 +0800

    q1-4 answers

--- app/src/main/java/edu/inf2007/lab01/MainActivity.kt
+++ app/src/main/java/edu/inf2007/lab01/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.inf2007.lab01
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import edu.inf2007.lab01.ui.theme.Lab01Theme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO: Task 6 here
+
+
+        // Get the current time in the specified format
+        val currentTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
+        Log.d(TAG,"Attack of the killer androids at $currentTime")
+
+
 
         setContent {
             Lab01Theme {
